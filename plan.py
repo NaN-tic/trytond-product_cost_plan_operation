4cd7fd8932c3c9c3bcec1e90ebac50a81c5330d0
?ngel ?lvarez <angel@nan-tic.com>
2014-01-04 19:31:49 +0100

    add correct way to manage workcenter and workcenter_category

--- plan.py
+++ plan.py
@@ -12,31 +12,38 @@ class PlanOperationLine(ModelSQL, ModelView):
     __name__ = 'product.cost.plan.operation_line'
 
     plan = fields.Many2One('product.cost.plan', 'Plan', required=True)
-    work_center = fields.Many2One('production.work_center', 'Work Center',
-        required=True)
+    work_center = fields.Many2One('production.work_center', 'Work Center')
+    work_center_category = fields.Many2One('production.work_center.category',
+        'Work Center Category')
     route_operation = fields.Many2One('production.route.operation',
         'Route Operation')
     uom_category = fields.Function(fields.Many2One(
             'product.uom.category', 'Uom Category', on_change_with=[
-                'work_center']),
+                'work_center', 'work_center_category']),
         'on_change_with_uom_category')
     uom = fields.Many2One('product.uom', 'Uom', required=True, domain=[
             ('category', '=', Eval('uom_category')),
-            ], depends=['uom_category'], on_change_with=['work_center'])
+            ], depends=['uom_category'], on_change_with=['work_center',
+            'work_center_category'])
     unit_digits = fields.Function(fields.Integer('Unit Digits',
             on_change_with=['uom']), 'on_change_with_unit_digits')
     quantity = fields.Float('Quantity', required=True,
         digits=(16, Eval('unit_digits', 2)), depends=['unit_digits'])
     cost = fields.Function(fields.Numeric('Cost', on_change_with=['quantity',
-                'cost_price', 'uom', 'work_center']), 'on_change_with_cost')
+                'cost_price', 'uom', 'work_center', 'work_center_category']),
+                'on_change_with_cost')
 
     def on_change_with_uom_category(self, name=None):
         if self.work_center:
             return self.work_center.uom.category.id
+        elif self.work_center_category:
+            return self.work_center_category.uom.category.id
 
     def on_change_with_uom(self):
         if self.work_center:
             return self.work_center.uom.id
+        if self.work_center_category:
+            return self.work_center_category.uom.id
 
     def on_change_with_unit_digits(self, name=None):
         if self.uom:
@@ -46,9 +53,10 @@ class PlanOperationLine(ModelSQL, ModelView):
     def on_change_with_cost(self, name=None):
         pool = Pool()
         Uom = pool.get('product.uom')
+        wc = self.work_center or self.work_center_category
         quantity = Uom.compute_qty(self.uom, self.quantity,
-            self.work_center.uom)
-        return Decimal(str(quantity)) * self.work_center.cost_price
+            wc.uom)
+        return Decimal(str(quantity)) * wc.cost_price
 
 
 class Plan:
@@ -66,8 +74,6 @@ class Plan:
             on_change_with=['operations']), 'on_change_with_operation_cost')
 
     def update_operations(self):
-        pool = Pool()
-        WorkCenter = pool.get('production.work_center')
         if not self.route:
             return {}
         operations = {
@@ -79,22 +85,20 @@ class Plan:
             }
         for operation in self.route.operations:
             work_center = None
+            work_center_category = None
             if operation.work_center:
                 work_center = operation.work_center
             elif operation.work_center_category:
-                centers = WorkCenter.search([
-                        ('category', '=', operation.work_center_category),
-                        ], limit=1)
-                if centers:
-                    work_center, = centers
+                work_center_category = operation.work_center_category
 
-            if not work_center:
-                self.raise_user_error('no_work_center', operation.rec_name)
+            wc = work_center or work_center_category
 
             operations['add'].append({
-                    'work_center': work_center.id,
+                    'work_center': work_center and work_center.id or None,
+                    'work_center_category': work_center_category and
+                        work_center_category.id or None,
                     'route_operation': operation.id,
-                    'uom': work_center.uom.id,
+                    'uom': wc.uom.id,
                     'quantity': 0.0,
                     })
         return changes
@@ -109,5 +113,5 @@ class Plan:
     def on_change_with_operation_cost(self, name=None):
         cost = Decimal('0.0')
         for operation in self.operations:
-            cost += operation.cost
+            cost += operation.cost or Decimal('0.0')
         return cost
