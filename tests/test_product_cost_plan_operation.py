f8c5fcb93220522ca61add6c2f75d6b95ea57b8a
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-07-12 13:17:37 +0200

    Update test to version 4.0

--- tests/test_product_cost_plan_operation.py
+++ tests/test_product_cost_plan_operation.py
@@ -4,23 +4,14 @@
 import unittest
 import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
-class TestCase(unittest.TestCase):
+class TestCase(ModuleTestCase):
     'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('product_cost_plan_operation')
-
-    def test0005views(self):
-        'Test views'
-        test_view('product_cost_plan_operation')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'product_cost_plan_operation'
 
 
 def suite():
@@ -29,5 +20,6 @@ def suite():
     suite.addTests(doctest.DocFileSuite(
             'scenario_product_cost_plan_operation.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
